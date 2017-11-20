3b7868efc207ea83a141cbe02ba8e0dec4925c61
Eric Levine <elevine@getty.edu>
2017-11-19 21:54:56 -0800

    Got rid of console logs

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,10 @@ window.addEventListener("hashchange", shiftWindow);
 let $airports_table = $("table");
 
 $(".airport_point").on('mouseover', function(e){
-	console.log(this);
 	let apt = this.dataset.airport;
-	console.log(apt);
 	$airports_table.find("tr").css("background-color", "inherit");
 	let $row = $airports_table.find("tr[data-airport='"+apt+"']");
-	console.log("row is ", $row);
 	if($row && $row.length > 0){
-		console.log("oh we found one");
 		$row.css("background-color", "#c3e0f7");
 	}
 });
