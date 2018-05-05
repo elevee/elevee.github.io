be0b5714792a7c4043b053b7f58c46324c91634a
Eric Levine <djlevine@gmail.com>
2018-05-05 01:43:50 -0700

    Saving status for now

--- scripts/index.js
+++ scripts/index.js
@@ -39,6 +39,7 @@ $(document).ready(function(){
 	 //    iconColor: '',
 	 //    rtl: false,
 	 //    top: null,
+	 //    setTop: 100,
 	 //    bottom: null,
 	 //    borderBottom: true,
 	 //    padding: 0,
